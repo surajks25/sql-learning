7624f8539bb36173dec10a74ae521710d8b499c4
surajks25 <surajkskulal@gmail.com>
2026-04-03 15:33:33 +0530

    INNER_LEFT_RIGHT_UNION_JOINS

--- day4.sql
+++ day4.sql
@@ -18,6 +18,8 @@ INSERT INTO marks VALUES
 (6, 60);
 
 SELECT * FROM marks;
+-- INNER JOIN → only matching rows
+-- LEFT JOIN → all left table rows
 
 -- INNER JOIN
 SELECT student.name, marks.marks
@@ -30,3 +32,59 @@ SELECT student.name, marks.marks
 FROM student
 LEFT JOIN marks
 ON student.id = marks.student_id;
+
+-- right join
+-- 👉 Show all data from marks table
+-- 👉 Even if student not found
+
+SELECT student.name, marks.marks
+FROM student
+RIGHT JOIN marks
+ON student.id = marks.student_id;
+
+-- FULL JOIN (using UNION) this will help to ignore the null value show all the table value usiing union
+SELECT student.name, marks.marks
+FROM student
+LEFT JOIN marks
+ON student.id = marks.student_id
+
+UNION
+
+SELECT student.name, marks.marks
+FROM student
+RIGHT JOIN marks
+ON student.id = marks.student_id;
+
+-- 1. JOIN with conditions (real-world filtering)
+SELECT student.name, marks.marks
+FROM student
+JOIN marks
+ON student.id = marks.student_id
+WHERE marks > 80;
+
+-- 2. JOIN + GROUP BY (very common in jobs)
+SELECT student.city, COUNT(marks.marks)
+FROM student
+JOIN marks
+ON student.id = marks.student_id
+GROUP BY student.city;
+
+-- Show student names with marks greater than 70
+SELECT student.name, marks.marks
+FROM student
+JOIN marks
+ON student.id = marks.student_id
+WHERE marks.marks > 70;
+
+-- Count number of students who have marks
+SELECT COUNT(*) AS total_students
+FROM student
+JOIN marks
+ON student.id = marks.student_id;
+
+-- Show city-wise count of students who have marks
+SELECT student.city, COUNT(*) AS total
+FROM student
+JOIN marks
+ON student.id = marks.student_id
+GROUP BY student.city;
